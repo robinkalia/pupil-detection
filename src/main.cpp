b9315de2c14a63ea3914e90f569086b01ce66dc9
volcanoh <huang.volcano@gmail.com>
2016-08-05 11:17:03 +0800

    gauss

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,145 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include "CCA_Labeling.h"
+#include <stack>
 using namespace cv;
 using namespace std;
-cv::Point p1, p2;
-cv::Rect rect;
-cv::Point point;
-cv::Mat  roi;
+#include <unistd.h>
+#include <stdio.h>
 
-int threshold_value1 = 22;
-int threshold_value2 = 1000;
-int threshold_value3 = 3000;
-int threshold_type = 1;
-int const max_value1 = 255;
-int const max_value2 = 3000;
-int const max_value3 = 3000;
-int const max_type = 4;
-int const max_BINARY_value = 255;
-
-Mat src, src_gray, dst;
-string window_name = "Threshold Demo";
-
-string trackbar_type = "Type";
-string trackbar_value1 = "Value";
-string trackbar_value2 = "min Area";
-string trackbar_value3 = "max Area";
-
-/// Function headers
-void update(const cv::Mat&);
-int avgGrayValue(const cv::Mat gray);
-
+cv::Mat computeMatXGradient(const cv::Mat &mat);
+/// + -> 1
+/// 0 -> 0
+/// - -> -1
+cv::Mat sign(const cv::Mat &mat);
+/// If the value of pixel is 0, assign it to the last pixel(left or up)
+cv::Mat reSign(const cv::Mat mat, int XorY); 
 int main( int argc, char** argv )
 {
-  /// Load vedio
+      /// Load vedio
   cv::VideoCapture cap(argv[1]);
-
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-
-  /// For binary threshold
-  createTrackbar( trackbar_value1,
-                  window_name, &threshold_value1,
-                  max_value1, NULL );
-  /// For max area
-  createTrackbar( trackbar_value2,
-                  window_name, &threshold_value2,
-                  max_value2, NULL );
-  /// For min area
-  createTrackbar( trackbar_value3,
-                  window_name, &threshold_value3,
-                  max_value3, NULL );
-
   /// update
   while(true)
-  {
-    cv::Mat frame;
-    cap >> frame;
-    if (frame.data == NULL) {
-      std::cout << "Finished!" << std::endl;
-      return 0;
-    }
-    cvtColor( frame, frame, CV_BGR2GRAY);
-    threshold_value1 = 0.3 * avgGrayValue(frame);
-    update(frame);
-    int c;
-    cv::imshow("frame", frame);
-    c = waitKey( 20 );
-    if( (char)c == 27 )
-      { break; }
-   }
+    {
+      cv::Mat frame;
+      cap >> frame;
+      if (frame.data == NULL) {
+        std::cout << "Finished!" << std::endl;
+        return 0;
+      }
+      cvtColor(frame, frame, CV_BGR2GRAY);
+      cv::Mat doubleMat;
+      frame.convertTo(doubleMat, CV_64F);
+      cv::Mat gauss;
+      cv::GaussianBlur(doubleMat, gauss, cv::Size(91, 91), 16);
 
+      // cv::medianBlur(doubleMat, gauss, 111);
+      int cols = frame.cols;
+      int rows = frame.rows;
+
+      cv::Mat gradientX = computeMatXGradient(gauss);
+      cv::Mat gradientY = computeMatXGradient(gauss.t()).t();
+      cv::Mat signX = sign(gradientX);
+      cv::Mat signY = sign(gradientY);
+      cv::Mat gradientReSignX = reSign(signX, 0); 
+      cv::Mat gradientReSignY = reSign(signY, 1); 
+      int * dataX = (int*)gradientReSignX.ptr();
+      int * dataY = (int*)gradientReSignY.ptr();
+      for (int x = 0; x  < cols; ++x) {
+        for (int y = 0; y < rows; ++y) {
+          if (dataX[y * cols + x] == 2 && dataY[y * cols + x] == 2  && frame.at<unsigned char>(y, x) < 15
+              )
+
+            cv::circle(frame, cv::Point(x, y), 1, 255);
+         }
+      }
+      int c;
+      cv::imshow("gauss", gauss);
+      cv::imshow("gradientX", gradientX);
+      cv::imshow("gradientY", gradientY);
+      cv::imshow("frame", frame);
+
+      c = waitKey(  10 );
+      if( (char)c == 27 )
+        { break; }
+    }
 }
 
-
-
-/// Image Process
-void update(const cv::Mat& frame) {
-
-  threshold(frame, dst, threshold_value1, max_BINARY_value,threshold_type );
-  cv::imshow("binary", dst);
-
-  cv::Mat cca_out = icvprCcaBySeedFill(dst / 255, threshold_value2, threshold_value3);
-  int elementSize = 3;
-  cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE,
-                                              cv::Size(2 * elementSize + 1, 2 * elementSize + 1),
-                                              cv::Point(elementSize, elementSize));
-
-  cv::Mat dilate_out, close_out;
-  cv::dilate(cca_out, dilate_out, element);
-  cv::erode(dilate_out, close_out, element);
-  cv::Mat inv_cca_out = icvprCcaBySeedFill((255 - close_out) / 255, 000, 20000);
-  cv::Mat add_out = close_out + inv_cca_out;
-
-  imshow( window_name, add_out);
-
-  cv::Mat dstImage = cv::Mat::zeros(add_out.rows, add_out.cols, CV_8UC3);
-  vector<vector<cv::Point> >contours;
-  vector<cv::Vec4i> hierarchy;
-  int index = 0;
-  cv::findContours(add_out, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-  if (contours.size() != 0) {
-    for (; index >= 0; index = hierarchy[index][0]) {
-      cv::Scalar color( rand()&255, rand()&255, rand()&255);
-      cv::drawContours(dstImage, contours, index, color, 1, 8, hierarchy);
+cv::Mat computeMatXGradient(const cv::Mat &mat) {
+  cv::Mat out(mat.rows,mat.cols,CV_64F);
+  for (int y = 0; y < mat.rows; ++y) {
+    const double *Mr = mat.ptr<double>(y);
+    double *Or = out.ptr<double>(y);
+    Or[0] = Mr[1] - Mr[0];
+    for (int x = 1; x < mat.cols - 1; ++x) {
+      Or[x] = Mr[x] - Mr[x - 1];
     }
   }
-  imshow( "dst", dstImage);
+  return out;
 }
 
-int avgGrayValue(const cv::Mat gray) {
-  int cols = gray.cols;
-  int rows = gray.rows;
-  double val = 0;
-  for (int i = 0; i < cols * rows; ++i) {
-     val += gray.data[i];
+cv::Mat sign(const cv::Mat &mat) {
+  int cols = mat.cols, rows = mat.rows;
+  cv::Mat result = cv::Mat::zeros(rows, cols, CV_32S);
+  int* result_val = (int *) result.ptr();
+  double* mat_val = (double *) mat.ptr();
+  for (int i = 0; i < rows * cols; ++i) {
+    if (mat_val[i] > 0.0000001) result_val[i] = 1;
+    else if (mat_val[i] < 0.0000001 && mat_val[i] > -0.0000001) result_val[i] = 0;
+    else result_val[i] = -1;
   }
-  return val / (cols * rows);
+  return result;
 }
+
+/// Input: Mat_<int>  -1 0 1
+cv::Mat reSign(const cv::Mat mat, int XorY) {
+  cv::Mat result = cv::Mat::zeros(mat.rows, mat.cols, CV_32S);
+  int cols = mat.cols;
+  int rows = mat.rows;
+  // Y
+  if (XorY == 1) {
+    for (int x = 1; x < cols; ++x) {
+      std::pair<int, int> yPair;
+      for (int y = 1; y < rows - 1; ++y) {
+        if (mat.at<int>(y, x) != 0) {
+          result.at<int>(y, x) = mat.at<int>(y, x) - mat.at<int>(y - 1, x);
+        }
+        else {
+          if (mat.at<int>(y - 1, x) != 0) {
+            yPair.first = y;
+          }
+          if (mat.at<int>(y + 1, x) != 0) {
+            yPair.second = y;
+            for (int i = yPair.first; i <= yPair.second; ++i) {
+              result.at<int>(i, x) = mat.at<int>(yPair.second + 1, x) - mat.at<int>(yPair.first - 1, x);
+            }
+          }
+        }
+      }
+    }
+  }
+  // X
+  else {
+    for (int y = 1;y < rows; ++y) {
+      std::pair<int, int> xPair;
+      for (int x = 1; x < cols - 1; ++x) {
+        if (mat.at<int>(y, x) != 0) {
+          result.at<int>(y, x) = mat.at<int>(y, x) - mat.at<int>(y, x - 1);
+        }
+        else {
+          if (mat.at<int>(y, x - 1) != 0) {
+            xPair.first = x - 1;
+          }
+          if (mat.at<int>(y, x + 1) != 0) {
+            xPair.second = x + 1;
+            for (int i = xPair.first; i <= xPair.second; ++i) {
+              result.at<int>(y, i) = mat.at<int>(y, xPair.second) - mat.at<int>(y, xPair.first);
+            }
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
